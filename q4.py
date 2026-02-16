5cfecfcab1dd547a7c7779767566243e3431d4bc
Anish-S-06 <anish.sandaka1806@gmail.com>
2026-02-16 22:26:05 +0530

    Completed q4.py

---
+++ q4.py
@@ -0,0 +1,11 @@
+#Team Name - Synapse_Squad
+def get_ticket_price ( age : int , is_student : bool ) -> int :
+ if age < 12 :
+  return 8
+ elif 12<= age <=64:
+    if is_student :
+     return 12
+    else :
+     return 15
+ else :
+    return 10
