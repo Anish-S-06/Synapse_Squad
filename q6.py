710762ba422df15a750876e38a96c4e4205544c5
Anish-S-06 <anish.sandaka1806@gmail.com>
2026-02-17 20:04:40 +0530

    Updated q6.py so that it also solves for lower case units

--- q6.py
+++ q6.py
@@ -1,7 +1,7 @@
 #Team Name - Synapse_Squad
-def convert_temperature(value : float,unit :str)->float:
+def convert_temperature(value : float,unit :str)->float | str:
     if unit.upper() == 'C':
-        return round((value *9/5)+32,1)
+        return round((value*9/5)+32,1)
     elif unit.upper() == 'F':
         return round((value-32)*5/9,1)
     else:
