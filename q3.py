03e43f11152df7f62152b0dcd8066589de1f8638
Anish-S-06 <anish.sandaka1806@gmail.com>
2026-02-17 19:40:24 +0530

    Updated q3.py

--- q3.py
+++ q3.py
@@ -1,11 +1,13 @@
 #Team Name - Synapse_Squad
 def average_passing_grades ( grades : list [ int ]) -> float :
- average=0.0
- count=0
+ sum = 0.0
+ count = 0
  for grade in grades :
         if grade >= 50 :
-            average += grade
+            sum += grade
             count += 1
  if count == 0 :
     return 0.0
- return average / count
+ average = sum/count
+ return average
+
