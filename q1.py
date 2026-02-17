227e585e3cea4d0087dc3dc54566a502225695d5
Anish-S-06 <anish.sandaka1806@gmail.com>
2026-02-17 21:06:16 +0530

    Updated q1.py

--- q1.py
+++ q1.py
@@ -1,6 +1,7 @@
 #Team name - Synapse_Squad
 def calculate_total_bill(amount: float, tip_percent: int) -> float:
     tip_percent = float(tip_percent)
+    amount = float(amount)
     tip = (amount * tip_percent) / 100
     total = tip + amount
     return round(total,2)
